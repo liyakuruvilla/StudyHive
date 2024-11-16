bbf9841218a9299dc24468080fa2437aaae86c01
tinajang01 <tinajang01@gmail.com>
2024-11-16 13:43:25 -0500

    rewrote code because run wasn't working

---
+++ studyhive/src/learning-style-quiz/quiz.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Quiz = () => {
+  return (
+    <div>
+      <h1>What's Your Learning Style?</h1>
+    <div id="quiz"></div>
+    <button id="submit">Submit</button>
+    <div id="result"></div>
+    </div>
+  )
+}
+
+export default Quiz
