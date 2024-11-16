6ff5807a59e7409ee477758c5e6ca7e062331e61
Nadeen <nadeenfindakly@gmail.com>
2024-11-16 15:28:59 -0500

    Adding backend

--- studyhive/src/index.js
+++ studyhive/src/index.js
@@ -15,3 +15,29 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const express = require('express');
+const cors = require('cors');
+const sql = require('mssql');
+const config = require('./dbConfig');
+const app = express();
+app.use(cors());
+app.use(express.json());
+sql.connect(config, (err) => {
+if (err) console.log(err);
+else console.log('Connected to SQL Server');
+});
+app.post('/login', async(req, res) => {
+const { username, password } = req.body;
+try {
+const result = await sql.query`INSERT INTO Users(username,
+password) VALUES (${username}, ${password})`;
+res.send({ message: 'You are logged in' });
+} catch(err) {
+console.log(err);
+res.status(500).send('Error saving to the database');
+}
+});
+app.listen(5000, () => {
+console.log('Server is running on port 5000');
+});
